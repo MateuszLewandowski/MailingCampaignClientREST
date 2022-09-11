286c958f33493d86522b649f9ba80fb19f17754b
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-11 19:27:39 +0200

    request var fix

--- src/kernel.php
+++ src/kernel.php
@@ -4,7 +4,8 @@ namespace MailingCampaign\Src;
 
 use Symfony\Component\HttpFoundation\Request;
 
-$GLOBALS['method'] = Request::createFromGlobals()->getMethod();
+$request = Request::createFromGlobals();
+$GLOBALS['method'] = $request->getMethod();
 $GLOBALS['headers'] = Request::getTrustedHeaderSet();
 // $GLOBALS['request_uri'] = str_replace('/ZadanieRekrutacyjneMerce/', '', $request->getRequestUri());
 $GLOBALS['request_uri'] = ltrim($request->getRequestUri(), '/');
