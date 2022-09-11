21117bac09826659c00cbf05f287c1ac36215349
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-11 17:39:45 +0200

    email validation

--- src/Requests/CreateCampaignRequest.php
+++ src/Requests/CreateCampaignRequest.php
@@ -29,6 +29,16 @@ class CreateCampaignRequest extends AbstractRequest
                 )
             );
         }
+        foreach ($this->mailing_list as $email) {
+            if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email)) {
+                ResponseMacroServiceProvider::api(
+                    status: Response::HTTP_BAD_REQUEST,
+                    result: [
+                        'error' => 'Mailing list can not be empty.',
+                    ]
+                    );
+            }
+        }
     }
 
     public function validated(): array 
