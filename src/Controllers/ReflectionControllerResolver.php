11c428b2cde12f6e3ea6c211f5be31761a762c80
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-11 15:09:30 +0200

    Auto dependency injection for constructors and methods

---
+++ src/Controllers/ReflectionControllerResolver.php
@@ -0,0 +1,55 @@
+<?php 
+
+namespace MailingCampaign\Src\Controllers;
+
+use MailingCampaign\Src\Repositories\CampaignRepository;
+use MailingCampaign\Src\Services\CampaignService;
+
+final class ReflectionControllerResolver
+{
+    public function resolveAutomaticDependencyInjection(string $class): object
+    {
+        $reflectionClass = new \ReflectionClass($class);
+        if (($constructor = $reflectionClass->getConstructor()) === null) {
+            return $reflectionClass->newInstance();
+        }
+        if (($params = $constructor->getParameters()) === []) {
+            return $reflectionClass->newInstance();
+        }
+        $newInstanceParams = [];
+        foreach ($params as $param) {
+            $newInstanceParams[] = $param->getClass() === null ? $param->getDefaultValue() : $this->resolveAutomaticDependencyInjection(
+                $this->interfacesSwapStrategy($param->getClass()->getName())
+            );
+        }
+        return $reflectionClass->newInstanceArgs(
+            $newInstanceParams
+        );
+    }
+
+    public function getMethodDependencies(string $class, string $method) {
+        $reflectionClass = new \ReflectionClass($class);
+        $reflectionMethod = $reflectionClass->getMethod($method);
+        $params = $reflectionMethod->getParameters();
+        
+        $newInstanceParams = [];
+        foreach ($params as $param) {
+            $class = $param->getClass()->getName();
+            $newInstanceParams[] = new $class;
+        }
+        return $newInstanceParams;
+    }
+
+    public function interfacesSwapStrategy(string $class) {
+        if (!str_contains($class, 'Interface')) {
+            return $class;
+        }
+        switch ($class = str_replace('MailingCampaign\\Src\\Interfaces\\', '', $class)) 
+        {
+            case 'ServiceInterface':
+                return CampaignService::class;
+            case 'RepositoryInterface':
+                return CampaignRepository::class;
+        }
+    }
+}
