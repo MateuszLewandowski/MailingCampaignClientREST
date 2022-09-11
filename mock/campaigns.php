0de72fd1cb5c5d6dfe1945220b1e5a2dd6aa720e
Mateusz Lewandowski <lewyy2501@gmail.com>
2022-09-11 15:13:18 +0200

    Mock data for testing purposes

---
+++ mock/campaigns.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+    [
+        'id' => 10,
+        'name' => 'Campaign 1',
+        'mailing_list' => [
+            'test1@test.pl', 'test1@test.pl', 'test1@test.pl', 'test1@test.pl',
+        ],
+    ],
+    [
+        'id' => 11,
+        'name' => 'Campaign 2',
+        'mailing_list' => [
+            'test1@test.pl', 'test1@test.pl', 
+        ],
+    ],
+];
